57e7df9d08f9e1a853512557cc834592b1728f77
parthkathale18 <150028426+parthkathale18@users.noreply.github.com>
2024-03-12 21:14:52 +0530

    Add files via upload

---
+++ sqr.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * sqr
+ */
+public class sqr {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("n:");
+        int n = sc.nextInt();
+        System.out.println("m:");
+        int m = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+    }
+}
